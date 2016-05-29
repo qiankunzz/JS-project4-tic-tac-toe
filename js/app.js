3a776698b7215949118e676bded01facdbed2060
qiankunzz <qiankunzz@gmail.com>
2016-05-29 19:28:20 -0400

    Fixed the bug!
    1. Disabled all buttons first before we bind click to each button, so
    that the event in the previous game is not passed into the next game.
    2. re-organized the logic so that ‘it’s a tie’ will not appear when a
    player won the game in 9 moves.
    3. made the code more DRY
    4. add more comments

--- js/app.js
+++ js/app.js
@@ -5,12 +5,16 @@ var buttons = $(".boxes").children();
   // assign values from 1 to 9 to all the 9 buttons respectively
   // Set an array of 9 arrays to define all the winning combinations
 var winningCombo = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-  // // counting array for both players counting their scores in each of the winningCombo.
-  // // if only one score reaches 3, then the player wins the game.
-var countingO = [];
-var countingX = [];
-var movePlayer1 = true;
-var moveCount = 0;
+var countingO = [0,0,0,0,0,0,0,0];
+var countingX = [0,0,0,0,0,0,0,0];
+    // counting array for both players counting their scores in each of the winningCombo.
+      // for example, the upper left button represents '1', when player1 made that move,
+      // '1' matches [1,2,3],[1,4,7],[1,5,9] in the winningCombo
+        // therefore, player1's counting become [1,0,0,1,0,0,1,0]
+          // when the maxium number in the array reaches 3, the player wins the game.
+
+var movePlayer1;      // This is a boolean that determines who moves
+var moveCount;        // This is to count the move, so we know when it's a tie
 
 // Reset the game to the orginal
 function gameReset() {
@@ -31,11 +35,16 @@ function gameReset() {
 }
 
 function buttonsReset() {
+  // !!! This is a very important step !!!
+  // After a game is finished, all the buttons need to be diabled, otherwise it will be passed into the next game.
+  buttons.off("click");
+  buttons.off('mouseenter mouseleave');
   for (n = 0; n < buttons.length; n++) {
   // bind the click event with
   //  1. function to record the progress of the game: createClickButtonMove
   //  2. function to set active colors: setActiveColor
-    buttons.eq(n).click(createClickButtonMove(n)).click(setActiveColor(n)).click(disableClick(n));
+    buttons.eq(n).click(createClickButtonMove(n)).click(setActiveColor(n));
+    buttons.eq(n).click(disableClick(n));
   // bind the hover event
     buttons.eq(n).hover(
       setHoverImage(n),setHoverImageOut(n)
@@ -84,49 +93,48 @@ function setActiveColor(n) {
   }
 }
 
+// When player1 is playing:
 // here "n+1" represent the value associated with the button (1-9)
-// When the button is clicked, the value is passed to the function to record the score in the counting array
+function playMove(countingO, n) {
+  // for a spedific button clicked, cycle the value through the winningCombo
+  for (i = 0; i < winningCombo.length; i++) {
+    // if the winningCombo has that value
+    if (winningCombo[i].includes(n+1)) {
+      // increase the counting score accordingly
+      countingO[i]++;
+    }
+  }
+  movePlayer1 = !movePlayer1;
+}
+
 function createClickButtonMove(n) {
   return function clickButton(){
     //  When Player1 is playing
     moveCount++;
     if (movePlayer1 === true) {
-      for (i = 0; i < winningCombo.length; i++) {
-        if (winningCombo[i].includes(n+1)) {
-          countingO[i]++;
-        }
-      }
-      movePlayer1 = false;
-  //    console.log("Player 1 " + Math.max(...winningCondition.countingO));
-      if (Math.max(...countingO) === 3) {
-        $("div header p.message").append("Winner!");
-        $("#finish").addClass("screen-win-one").show();
-      }
-    } else
-    //  When Player2 is playing (the code here is not DRY enough)
-    {
-      for (i = 0; i < winningCombo.length; i++) {
-        if (winningCombo[i].includes(n+1)) {
-          countingX[i]++;
-        }
-      }
-      movePlayer1 = true;
-    //  console.log("Player 2 " + Math.max(...winningCondition.countingX));
-      if (Math.max(...countingX) === 3) {
-        $("div header p.message").append("Winner!");
-        $("#finish").addClass("screen-win-two").show();
-      }
+      playMove(countingO, n);
+    } else {
+      playMove(countingX, n);
     }
-    // Show tie screen
-    if (moveCount === 9)
+    // 3 game finish conditions:
+    // player1 wins
+    if (Math.max(...countingO) === 3) {
+      $("div header p.message").append("Winner!");
+      $("#finish").addClass("screen-win-one").show();
+    }
+    // Player2 wins
+    else if (Math.max(...countingX) === 3) {
+      $("div header p.message").append("Winner!");
+      $("#finish").addClass("screen-win-two").show();
+    }
+    // Ties
+    else if (moveCount === 9)
     {
       $("div header p.message").append("It's a tie!");
       $("#finish").addClass("screen-win-draw").show();
     }
-    console.log(movePlayer1);
   }
 };
-
 
 // Reset the game after each time the button is clicked
 $(".button").click(function(){
@@ -134,7 +142,7 @@ $(".button").click(function(){
   $("#start").hide();
   $("#board").show();
   gameReset();
-  buttonsReset()
+  buttonsReset();
 });
 
 }();
